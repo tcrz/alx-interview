8bcff23687c3391b086194bb4da4af186183ac8e
Tonycrz <tonybaidoo0@gmail.com>
2022-06-21 16:15:46 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -23,14 +23,14 @@ def canUnlockAll(boxes):
     for box in boxes:
         # loop through each key in a box for later comparison
         for key in box:
-           # loop through length of boxes list to compare index with key
+            # loop through length of boxes list to compare index with key
             for i in range(len(boxes)):
-                # if box is not current box and key is equal to an index in boxes list,
-                # append key to keys list
+                # if box is not current box and key is equal to an index in -
+                # boxes list, append key to keys list
                 if box != boxes[i] and key == i:
                     keys.append(key)
     # check if indexList is equal to unique list of keys, if true,
     # all keys have been found and  all boxes can be opened
-    if len(indexList) == len(list(set(keys))):
+    if indexList == list(set(keys)):
         return True
     return False
