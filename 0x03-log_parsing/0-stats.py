9447ac589a89c321f1d61398a7aa136c736d18c1
Tonycrz <tonybaidoo0@gmail.com>
2022-07-05 20:39:01 +0000

    diff_approach

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,40 +3,71 @@
 import sys
 
 
-def split_str(stdin):
-    str_list = stdin.split()
-    status_code = str_list[-2]
-    file_size = str_list[-1]
-    return {'status_code': status_code, 'file_size': int(file_size)}
+# def split_str(stdin):
+#     str_list = stdin.split()
+#     # print(len(str_list))
+#     if len(str_list) < 6:
+#         return None
+#     status_code = str_list[-2]
+#     file_size = str_list[-1]
+#     return {'status_code': status_code, 'file_size': int(file_size)}
 
 
-if __name__ == '__main__':
-    count = 0
-    status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    status_code_data = {code: 0 for code in status_codes}
-    file_size = 0
-    try:
-        for line in sys.stdin:
-            count += 1
-            if len(str_list) < 6:
-                data = split_str(line)
-                status_code_data[data['status_code']] += 1
-                sorted_code_data = dict(sorted(status_code_data.items()))
-                file_size += data['file_size']
-                if count % 10 == 0:
-                    print('File size: {}'.format(file_size))
-                    for k, v in sorted_code_data.items():
-                        if v != 0:
-                            print('{}: {}'.format(k, v))
-    except KeyboardInterrupt:
-        print('File size: {}'.format(file_size))
-        sorted_code_data = dict(sorted(status_code_data.items()))
-        for k, v in sorted_code_data.items():
-            if v != 0:
-                print('{}: {}'.format(k, v))
-    else:
-        print('File size: {}'.format(file_size))
-        sorted_code_data = dict(sorted(status_code_data.items()))
-        for k, v in sorted_code_data.items():
-            if v != 0:
-                print('{}: {}'.format(k, v))
+# if __name__ == '__main__':
+#     count = 0
+#     status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+#     status_code_data = {code: 0 for code in status_codes}
+#     file_size = 0
+#     try:
+#         for line in sys.stdin:
+#             count += 1
+#             data = split_str(line)
+#             if data is not None:
+#                 status_code_data[data['status_code']] += 1
+#                 sorted_code_data = dict(sorted(status_code_data.items()))
+#                 file_size += data['file_size']
+#                 if count % 10 == 0:
+#                     print('File size: {}'.format(file_size))
+#                     for k, v in sorted_code_data.items():
+#                         if v != 0:
+#                             print('{}: {}'.format(k, v))
+#     except KeyboardInterrupt:
+#         print('File size: {}'.format(file_size))
+#         sorted_code_data = dict(sorted(status_code_data.items()))
+#         for k, v in sorted_code_data.items():
+#             if v != 0:
+#                 print('{}: {}'.format(k, v))
+#     else:
+#         print('File size: {}'.format(file_size))
+#         sorted_code_data = dict(sorted(status_code_data.items()))
+#         for k, v in sorted_code_data.items():
+#             if v != 0:
+#                 print('{}: {}'.format(k, v))
+
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_code_data = {code: 0 for code in status_codes}
+count = 0
+total_file_size = 0
+try:
+    for line in sys.stdin:
+        count += 1
+        splitstr = line.split()
+        if len(splitstr) == 9:
+            total_file_size += int(splitstr[-1])
+            code = splitstr[-2]
+            status_code_data[code] += 1
+        if count % 10 == 0:
+            print('File size: {}'.format(total_file_size))
+            for k, v in sorted(status_code_data.items()):
+                if v != 0:
+                    print('{}: {}'.format(k, v))
+except KeyboardInterrupt:
+    print('File size: {}'.format(total_file_size))
+    for k, v in sorted(status_code_data.items()):
+        if v != 0:
+            print('{}: {}'.format(k, v))
+else:
+    print('File size: {}'.format(total_file_size))
+    for k, v in sorted(status_code_data.items()):
+        if v != 0:
+            print('{}: {}'.format(k, v))
