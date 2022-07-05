8ba96907d44be8aa1aa05fd642a86ad3ecaef45b
Tonycrz <tonybaidoo0@gmail.com>
2022-07-05 20:57:45 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -44,25 +44,23 @@ import sys
 #             if v != 0:
 #                 print('{}: {}'.format(k, v))
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 status_code_data = {code: 0 for code in status_codes}
-
 total_file_size = 0
 try:
     count = 0
     for line in sys.stdin:
+        count += 1
         splitstr = line.split()
-        if len(splitstr) > 6:
+        if len(splitstr) == 9:
             total_file_size += int(splitstr[-1])
-            code = splitstr[-2]
-            if code in status_code_data:
-                count += 1
-                status_code_data[code] += 1
-                if count % 10 == 0:
-                    print('File size: {}'.format(total_file_size))
-                    for k, v in sorted(status_code_data.items()):
-                        if v != 0:
-                            print('{}: {}'.format(k, v))
+            code = int(splitstr[-2])
+            status_code_data[code] += 1
+            if count % 10 == 0:
+                print('File size: {}'.format(total_file_size))
+                for k, v in sorted(status_code_data.items()):
+                    if v != 0:
+                        print('{}: {}'.format(k, v))
 except KeyboardInterrupt:
     print('File size: {}'.format(total_file_size))
     for k, v in sorted(status_code_data.items()):
