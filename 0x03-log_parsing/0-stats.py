9befd3481683424e972aec53d1e273c23ac67956
Tonycrz <tonybaidoo0@gmail.com>
2022-07-05 20:54:07 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -46,9 +46,10 @@ import sys
 
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 status_code_data = {code: 0 for code in status_codes}
-count = 0
+
 total_file_size = 0
 try:
+    count = 0
     for line in sys.stdin:
         splitstr = line.split()
         if len(splitstr) > 6:
