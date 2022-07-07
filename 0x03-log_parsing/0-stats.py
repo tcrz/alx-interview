b8346165102219c5585028813569c686a737bedb
Tonycrz <tonybaidoo0@gmail.com>
2022-07-07 18:36:48 +0000

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,15 @@
 """log parsing"""
 import sys
 
+
+def print_data(total_file_size, status_code_data):
+    """prints total size and status code count"""
+    print('File size: {}'.format(total_file_size))
+    for k, v in sorted(status_code_data.items()):
+        if v != 0:
+            print('{}: {}'.format(k, v))
+
+
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 status_code_data = {code: 0 for code in status_codes}
 total_file_size = 0
@@ -16,20 +25,11 @@ try:
                 count += 1
                 status_code_data[code] += 1
                 if count % 10 == 0:
-                    print('File size: {}'.format(total_file_size))
-                    for k, v in sorted(status_code_data.items()):
-                        if v != 0:
-                            print('{}: {}'.format(k, v))
-        except:
+                    print_data(total_file_size, status_code_data)
+        except IndexError:
             pass
 except KeyboardInterrupt:
-    print('File size: {}'.format(total_file_size))
-    for k, v in sorted(status_code_data.items()):
-        if v != 0:
-            print('{}: {}'.format(k, v))
+    print_data(total_file_size, status_code_data)
     raise
 else:
-    print('File size: {}'.format(total_file_size))
-    for k, v in sorted(status_code_data.items()):
-        if v != 0:
-            print('{}: {}'.format(k, v))
+    print_data(total_file_size, status_code_data)
