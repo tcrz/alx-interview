4f69b0b87c02060acb10f042dc985b761422814b
Tonycrz <tonybaidoo0@gmail.com>
2022-08-01 19:01:42 +0000

    [Doc]: add docstring to function

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+
+Prototype: def rotate_2d_matrix(matrix):
+Do not return anything. The matrix must be edited in-place.
+You can assume the matrix will have 2 dimensions and will not be empty.
+"""
+
 
 def rotate_2d_matrix(matrix):
     for i in range(len(matrix)):
@@ -7,4 +15,3 @@ def rotate_2d_matrix(matrix):
             matrix[i][j], matrix[j][i] = b, a
     for row in matrix:
         row.reverse()
-
