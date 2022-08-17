0e736ba08d4621c5ee476876a3cc947b8dbead8a
Tonycrz <tonybaidoo0@gmail.com>
2022-08-17 00:26:15 +0000

    [fix] iteration

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,7 @@ def island_perimeter(grid):
     """island perimenter function"""
     result = 0
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 result += 4
                 if i > 0 and grid[i-1][j] == 1:
