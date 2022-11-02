2b75cc1e1d04d387ef6613a05ed5f6b8e3617649
Tonycrz <tonybaidoo0@gmail.com>
2022-11-02 16:45:20 +0000

    clearer approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,13 +17,13 @@ def pascal_triangle(n):
     # First list already added, number of new lists needed is n - 1
     for x in range(n-1):
         # Add zeros at the beginning and ending of last sublist in
-        # final_list(makes for easier addition for where values
+        # final_list(makes for possible addition for where values
         # are non-existent)
-        temp = [0] + final_list[-1] + [0]
+        temp = [0] + final_list[x] + [0]
         # Next list to be added to final_list
         new_list = []
         # Length of the next row (length of the last sublist + 1)
-        for y in range(len(final_list[-1]) + 1):
+        for y in range(len(temp) - 1):
             # Necessary addition on values of current row/list and
             # appended to next list/row
             new_list.append(temp[y] + temp[y+1])
